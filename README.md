89688855a49bbcc62520c3611b6b51937ca03ee5
Tony <lin1000@gmail.com>
2017-08-27 16:59:36 +0800

    Init Hackerrank Challenges

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Hackerrank
 Fun Programming Challenges
+
+- [x] https://www.hackerrank.com/challenges/java-stdin-and-stdout-1
+- [x] https://www.hackerrank.com/challenges/java-if-else?h_r=next-challenge&h_v=zen
---
+++ src/main/java/demo/lin1000/hackerrank/StdinStdout/Solution.java
@@ -0,0 +1,12 @@
+package demo.lin1000.hackerrank.StdinStdout;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);        
+        while(scan.hasNextInt()){
+            System.out.println(scan.nextInt());
+        }
+    }
+}
